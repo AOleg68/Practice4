7280cb315c3d577dea2f7652007a04dfb8416f85
Oleg <o.antonenko2011@yandex.ru>
2022-11-03 12:06:40 +0300

    Задача 1

---
+++ Задача 1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Stepen (int a, int b)
+{
+     int result = 1;
+     for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);     
+
+}
+
+Console.WriteLine ("Введите число A:   ");
+int A = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Введите число B:   ");
+int B = Convert.ToInt32 (Console.ReadLine());
+
+Stepen (A, B);
